2db4db59e3faf95256400f9c4b1a3ad7e1fa8c8f
arusha <arusha.sharma2001@gmail.com>
2022-01-23 17:56:45 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const Task = require('./models/task')
+const taskRoutes = require('./routes/taskRoutes')
+
+const app = express()
+
+const dbURI =
+  'mongodb+srv://arusha:STIGE@node-tuts.ozzmw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    //console.log("result: of DBURI:- ", result);
+    app.listen(process.env.PORT || 5000)
+  })
+  .catch((err) => console.log(err))
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+  res.redirect('/tasks')
+})
+
+app.use(taskRoutes)
+// app.use('/tasks', taskRoutes);
